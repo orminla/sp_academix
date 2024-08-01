238cfaa2bae29d53e26edb0929ab5d5ed77961c3
langkingz <gilanglibna@gmail.com>
2024-08-01 11:02:52 +0700

    sudah slicing form

--- lib/views/sirekap/views/revisi/pages.dart
+++ lib/views/sirekap/views/revisi/pages.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:dotted_border/dotted_border.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class PagesFormRevisi extends StatefulWidget {
@@ -10,6 +14,22 @@ class PagesFormRevisi extends StatefulWidget {
 }
 
 class _PagesFormRevisiState extends State<PagesFormRevisi> {
+  String? _selectedValue;
+  final List<String> _option = ['izin', 'sakit', 'alpha'];
+
+  bool checked = false;
+
+  selectFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['jpg', 'png', 'jpeg', 'pdf'],
+    );
+
+    if (result != null) {
+      print('file picked : ${result.files.single.path}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +47,7 @@ class _PagesFormRevisiState extends State<PagesFormRevisi> {
             padding: const EdgeInsets.all(30),
             margin: const EdgeInsets.all(20),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Nim(),
                 NamaMahasiswa(),
@@ -35,9 +56,9 @@ class _PagesFormRevisiState extends State<PagesFormRevisi> {
                 const SizedBox(
                   height: 10,
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: const Column(
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
                     children: [
                       Text(
                           'Ukuran file maksimum: 3 mb, Jumlah maksimum file: 1'),
@@ -45,17 +66,174 @@ class _PagesFormRevisiState extends State<PagesFormRevisi> {
                   ),
                 ),
                 Container(
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color(0xffE7E7E7),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                   width: double.infinity,
                   child: DottedBorder(
-                    
-                    child: Text("File"),
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(20),
+                    color: Colors.black,
+                    padding: const EdgeInsets.all(10),
+                    child: Container(
+                      // color: Colors.white,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)),
+
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 3),
+                      child: TextButton(
+                        onPressed: () {
+                          selectFile();
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.cloud_upload,
+                              // color: Colors.grey,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            const Text(
+                              'Upload File',
+                              // style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                persyaratan(),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Waktu yang tersisa untuk mengunggah: ",
+                  style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '5 Hari',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: Colors.red,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                _Status(),
+                const SizedBox(
+                  height: 20,
+                ),
+                deskirpsi(),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: checked,
+                      onChanged: (e) {
+                        setState(() {
+                          checked = e!;
+                        });
+                      },
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Saya Menyatakan bahwa surat ini dibuat dengan sebenar-benarnya, saya siap menerima sanksi yang ada jika terdapat kecurangan",
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color(0xff158AD4),
+                          Color(0xFF39EADD),
+                        ]),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 50,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Kirim",
+                      style: GoogleFonts.poppins(color: Colors.white),
+                    ),
                   ),
                 )
               ],
             ),
-          )
+          ),
         ],
       ),
+    );
+  }
+
+  Row _Status() {
+    return Row(
+      children: [
+        Text(
+          "Ubah Status Kehadiran :",
+          style: GoogleFonts.poppins(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            color: Color(0xffe7e7e7),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: DropdownButton(
+              hint: Text("Status"),
+              value: _selectedValue,
+              items: _option
+                  .map((e) => DropdownMenuItem(value: e, child: Text(e)))
+                  .toList(),
+              onChanged: (String? e) {
+                setState(() {
+                  _selectedValue = e;
+                });
+              }),
+        )
+      ],
+    );
+  }
+
+  Column persyaratan() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Jenis File yang Diterima :"),
+        Text(".png .jpg .jpeg .pdf"),
+      ],
     );
   }
 
@@ -65,13 +243,13 @@ class _PagesFormRevisiState extends State<PagesFormRevisi> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Keterangan :",
+          const Text("Keterangan :",
               style: TextStyle(
                   fontSize: 15,
                   color: Colors.black,
                   fontWeight: FontWeight.bold)),
           TextFormField(
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               hintText: "Masukkan NIM",
               hintStyle: TextStyle(color: Colors.grey),
             ),
@@ -87,13 +265,13 @@ class _PagesFormRevisiState extends State<PagesFormRevisi> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Mata Kuliah :",
+          const Text("Mata Kuliah :",
               style: TextStyle(
                   fontSize: 15,
                   color: Colors.black,
                   fontWeight: FontWeight.bold)),
           TextFormField(
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               hintText: "Masukkan NIM",
               hintStyle: TextStyle(color: Colors.grey),
             ),
@@ -109,14 +287,36 @@ class _PagesFormRevisiState extends State<PagesFormRevisi> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Nama Mahasiswa :",
+          const Text("Nama Mahasiswa :",
               style: TextStyle(
                   fontSize: 15,
                   color: Colors.black,
                   fontWeight: FontWeight.bold)),
           TextFormField(
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               hintText: "Masukkan NIM",
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding deskirpsi() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Deskripsi :",
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold)),
+          TextFormField(
+            decoration: const InputDecoration(
+              // hintText: "Masukkan NIM",
               hintStyle: TextStyle(color: Colors.grey),
             ),
           ),
@@ -131,13 +331,13 @@ class _PagesFormRevisiState extends State<PagesFormRevisi> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("NIM :",
+          const Text("NIM :",
               style: TextStyle(
                   fontSize: 15,
                   color: Colors.black,
                   fontWeight: FontWeight.bold)),
           TextFormField(
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               hintText: "Masukkan NIM",
               hintStyle: TextStyle(color: Colors.grey),
             ),
@@ -149,7 +349,7 @@ class _PagesFormRevisiState extends State<PagesFormRevisi> {
 
   Container header() {
     return Container(
-      padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
       child: const Center(
         child: Text(
           "Revisi Presensi",
